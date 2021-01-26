f95398e64242e9304a07cb39443c22e7f1722429
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2021-01-26 16:46:31 +0300

    Update Q2_Full.py

--- Q2_Full.py
+++ Q2_Full.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Tue Jan 26 16:28:52 2021
+
+@author: basakulcay
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Jan 26 16:14:40 2021
 
 @author: basakulcay
@@ -17,39 +25,55 @@ def main():
         
     outfile.close()
     print('Scores are written')
+    fetch_grades()
+  
     
-main()
-
-def determine_grade():
-    total=0
-    count = len(open('scores.txt').readlines())
+def fetch_grades():
     print('Score\tGrade')
     print('--------------')
+    total = 0
+    count = 0
     infile = open('scores.txt', 'r')
     for line in infile:
-        
         grade=line.rstrip('\n')
-        total=total+int(grade)
+        determine_grade(grade)
+        total += int(grade)
+        count +=1
+      
+    #print('The total score is',total)
+    #print(count)
+    
+    
+    print('The score average of the scores:',total/count)     
+
+
         
-        if int(grade) >= 90:
-            print(grade, "\t\tAA")
-        elif int(grade) >= 85:
-            print(grade, "\t\tBA")
-        elif int(grade) >= 80:
-            print(grade, "\t\tBB")
-        elif int(grade) >= 75:
-            print(grade, "\t\tCB")
-        elif int(grade) >= 70:
-            print(grade, "\t\tCC")
-        elif int(grade) >= 65:
-            print(grade, "\t\tDC")
-        elif int(grade) >= 60:
-            print(grade, "\t\tDD")
-        else:
-            print(grade, "\t\tFF")
+    
+def determine_grade(grade):
+
+    
+    if int(grade) >= 90:
+        print(grade, "\t\tAA")
+    elif int(grade) >= 85:
+        print(grade, "\t\tBA")
+    elif int(grade) >= 80:
+        print(grade, "\t\tBB")
+    elif int(grade) >= 75:
+        print(grade, "\t\tCB")
+    elif int(grade) >= 70:
+        print(grade, "\t\tCC")
+    elif int(grade) >= 65:
+        print(grade, "\t\tDC")
+    elif int(grade) >= 60:
+        print(grade, "\t\tDD")
+    else:
+        print(grade, "\t\tFF")
             
     #print('The total score is',total)
     #print(count)
-    print('The score average of the scores:',total/count)
-
-determine_grade()
+    
+    
+    #print('The score average of the scores:',total/count)
+    
+    
+main()
